8f3f776c1eb4a68503694e6a031876dd218d6b81
DevTheo <jkimble@gmail.com>
2023-06-18 20:31:16 -0400

    Update trastpiler to use deno enabled one (#3)
    * Small tweak for transpiler
    
    * Updating to use Deno enabled trastpiler

--- src/transpile.ts
+++ src/transpile.ts
@@ -1,4 +1,4 @@
-import createTranspiler from "https://esm.sh/trastpiler@1.0.2";
+import createTranspiler from "https://raw.githubusercontent.com/DevTheo/deno-trastpiler/v1.0.4pre1/src/mod.ts";
 import * as globalMappers from "./syntax-tree/index.ts";
 
 export function createJspiclTranspiler(
